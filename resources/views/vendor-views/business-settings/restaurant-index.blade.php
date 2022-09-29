f8c86b0dd6a01421b6af37160e083872a140ea52
Soslan Gassiev <alfa_romeo01@mail.ru>
2022-09-29 16:36:30 +0300

    Убрал лишние настройки из конфигурации ресторана в админке ресторана

--- resources/views/vendor-views/business-settings/restaurant-index.blade.php
+++ resources/views/vendor-views/business-settings/restaurant-index.blade.php
@@ -86,7 +86,7 @@
                 </div>
             </div>
         </div>
-        <!-- End Page Header -->
+        <!-- End Page Header 
         <div class="card mb-3">
             <div class="card-header">
                 <h5 class="card-title">
@@ -240,7 +240,7 @@
                     </div>
                 </form>
             </div>
-        </div>
+        </div>-->
         <div class="card">
             <div class="card-header">
                 <h5 class="card-title">
